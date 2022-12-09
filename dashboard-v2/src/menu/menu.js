a3a7ab8ff551ee155b9ff7f3d469792dad20fd61
shailendraO4S <102019685+shailendraO4S@users.noreply.github.com>
2022-12-09 13:59:13 +0530

    ++Routes-for-e-warranty-added

--- dashboard-v2/src/menu/menu.js
+++ dashboard-v2/src/menu/menu.js
@@ -2,116 +2,43 @@ import { addDefaultAppMenus } from "o4s-ui/utils";
 
 const menus = [
   {
-    name: "App",
-    path: "/",
-    exact: true,
-    icon: "pi-check",
-    expand: true,
-    label: "Application",
+    name: "BrandProtectionAndEWarranty",
+    path: "/BrandProtectionAndEWarranty",
+    label: "Brand Protection & E-Warranty",
+    icon: "pi-book",
+    //component: () => import("@/views/BrandProtectionAndEWarranty/index.vue"),
+
     children: [
+      // {
+      //   name: "consumerScans",
+      //   path: "/consumerScans",
+      //   exact: true,
+      //   icon: "pi-circle",
+      // },
       {
-        name: "components",
-        path: "/components",
-
-        icon: "pi-file",
-        // component: () => import("@/views/generic-component/Index.vue"),
-
+        name: "eWarranty",
+        path: "/eWarranty",
+        exact: true,
+        //component: () => import("@/views/BrandProtectionAndEWarranty/eWarranty/index.vue"),
         children: [
           {
-            name: "button",
-            path: "/button",
-
+            name: "eWarrantyDashboard",
+            path: "/eWarrantyDashboard",
             exact: true,
-            icon: "pi-car",
-            component: () => import("@/views/generic-component/Index.vue"),
+            visible: false,
+            component: () => import("@/views/BrandProtectionAndEWarranty/eWarranty/eWarrantyDashboard/index.vue"),
           },
           {
-            name: "Others",
-            path: "/others",
+            name: "eWarrantyForm",
+            path: "/eWarrantyForm",
             exact: true,
-
-            // component: () => import("@/views/generic-component/Index.vue"),
-            // component: () => import("@/views/text/TextDemo.vue"),
-            children: [
-              {
-                name: "buttonprimary",
-                path: "/primary",
-                exact: true,
-                icon: "pi-car",
-                component: () => import("@/views/generic-component/Index.vue"),
-                children: [
-                  {
-                    name: "deep",
-                    path: "/deep1",
-                    exact: true,
-                    icon: "pi-amazon",
-                    component: () =>
-                      import("@/views/generic-component/Index.vue"),
-                  },
-                  {
-                    name: "added new page",
-                    path: "/newpage",
-                    exact: true,
-                    icon: "pi-spinner",
-                    component: () =>
-                      import("@/views/generic-component/Index.vue"),
-                  },
-                ],
-              },
-              {
-                name: "buttonsecondary",
-                path: "/secondary",
-                exact: true,
-                icon: "pi-car",
-                component: () => import("@/views/generic-component/Index.vue"),
-              },
-              // {
-              //   name: "buttonplain",
-              //   path: "/plain",
-              //   exact: true,
-              //   icon: "pi-file",
-              //   component: () => import("@/views/generic-component/Index.vue"),
-              // },
-            ],
-          },
-        ],
-      },
-      {
-        name: "InputText",
-        path: "/o4s-labeled-input-text",
-        component: () => import("@/views/generic-component/Index.vue"),
-      },
-      {
-        name: "Dropdown",
-        // path: "https://dev-dashboard.o4s.io/login",
-        url: "https://dev-dashboard.o4s.io/login",
-        icon: "pi pi-file",
-        component: () => import("@/views/generic-component/Index.vue"),
-      },
-      {
-        name: "Checkbox",
-        path: "/checkbox",
-        component: () => import("@/views/generic-component/Index.vue"),
-      },
-      {
-        name: "Paginator",
-        path: "/paginator",
-        component: () => import("@/views/generic-component/Index.vue"),
-      },
-      {
-        name: "Tooltip",
-        path: "/tooltip",
-        component: () => import("@/views/generic-component/Index.vue"),
-      },
+            visible: false,
+            component: () => import("@/views/BrandProtectionAndEWarranty/eWarranty/eWarrantyRequestForm/index.vue"),
+          }
+        ]
+      }
     ],
-    // children: [
-    //   {
-    //     name: "InputText",
-    //     path: "/o4s-labeled-input-text",
-    //     component: () => import("@/views/generic-component/Index.vue"),
-    //   },
-    // ],
-  },
+  }
 ];
 
 export default addDefaultAppMenus(menus);
