25b408519bb9805a253319c5e08a5a08752bf531
shailendraO4S <102019685+shailendraO4S@users.noreply.github.com>
2022-12-09 15:38:29 +0530

    ++Axios-setup

---
+++ dashboard-v2/src/api/eWarranty.js
@@ -0,0 +1,36 @@
+import { httpClient } from '../shared/httpClient'
+
+const fetchEWarrantyList = async () => {
+    try {
+      const fetchEWarrantyListUrl = `/consumer/warranty-list`
+      const eWarrantyList = await httpClient.post(fetchEWarrantyListUrl, {
+        "pageNumber": 10,
+        "pageSize": 0,
+        "filters": {
+            "status": "APPROVED"
+        }
+      })
+      return eWarrantyList
+    } catch (error) {
+        console.log("L3")
+      return error ? {errorCode: error.status, errorMessage: error.statusText} : error
+    }
+}
+
+const fetchWarranty = async () => {
+    try {
+        console.log("L4")
+        const fetchWarrantyUrl = `/consumer/warranty?code=F80F3435`
+        const eWarranty = await httpClient.get(fetchWarrantyUrl)
+        console.log("L5", eWarranty)
+        return eWarranty
+      } catch (error) {
+        console.log("L6")
+        return error ? {errorCode: error.status, errorMessage: error.statusText} : error
+      }
+}
+
+export default {
+    fetchEWarrantyList,
+    fetchWarranty
+}
