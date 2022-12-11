da905773c87c23f0cc5606a1f06c38f6c2772082
Maheshwaran <maheshwaran.gopal@original4sure.com>
2022-12-11 13:17:37 +0530

    menu bar issue fix

--- dashboard-v2/src/App.vue
+++ dashboard-v2/src/App.vue
@@ -1,12 +1,28 @@
 <script setup>
-import { ref, reactive } from "vue";
-import { RouterView } from "vue-router";
+import { ref, reactive, onMounted, watch } from "vue";
+import { RouterView, useRoute } from "vue-router";
+import { O4SText } from "o4s-ui";
+import "./App.scss";
 // import OUMenuBar from "o4s-ui/utils/components/ou-menu-bar/OUMenuBar.vue";
 // import OUMenuBreadcrumb from "o4s-ui/utils/components/ou-menu-breadcrumb/OUMenuBreadcrumb.vue";
 import { OUMenuBar, OUMenuBreadcrumb } from "o4s-ui";
 import menuobj from "./menu/menu";
 const menu = reactive(menuobj);
 const activeNavigationList = ref([]);
+const appMenuBar = ref(null);
+onMounted(() => {
+  alert("dashbaord");
+  console.log("Dashbaord watched " + appMenuBar.value.$el.tagName);
+  // appMenuBar.value.routeChange({ matched: [] });
+});
+
+const route = useRoute();
+watch(route, (route) => {
+  if (appMenuBar.value) {
+    appMenuBar.value.routeChange(route);
+  }
+});
+
 const onNavigationChange = (activeNavigations) => {
   alert();
   console.log("Example : " + JSON.stringify(activeNavigations));
@@ -17,7 +33,11 @@ const onNavigationChange = (activeNavigations) => {
 <template>
   <div class="root-layout overflow-hidden">
     <div class="side-menu">
-      <OUMenuBar @change="onNavigationChange" v-model:menu="menu" />
+      <OUMenuBar
+        @onChange="onNavigationChange"
+        v-model:menu="menu"
+        ref="appMenuBar"
+      />
     </div>
     <div class="root-container o-bg-secondary-100">
       <div>
@@ -27,15 +47,3 @@ const onNavigationChange = (activeNavigations) => {
     </div>
   </div>
 </template>
-<script>
-/* eslint-disable vue/no-unused-components */
-import { O4SText } from "o4s-ui";
-import "./App.scss";
-export default {
-  name: "App",
-  components: {
-    O4SText,
-  },
-};
-</script>
-<style scoped></style>
