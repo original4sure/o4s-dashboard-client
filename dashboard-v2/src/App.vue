27acfb85dae1d3f230161ac563640814aff19e61
Maheshwaran <maheshwaran.gopal@original4sure.com>
2022-12-14 22:40:26 +0530

    dashboard integration code

--- dashboard-v2/src/App.vue
+++ dashboard-v2/src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
-import { ref, reactive, watch } from "vue";
+import { ref, reactive, watch, onMounted } from "vue";
 import { useRoute } from "vue-router";
 import { RouterView } from "vue-router";
 import { O4SAppMenu } from "o4s-ui";
+import { useAuthUserStore } from "./store/user/auth";
 import menuobj from "./menu";
+import { removeTokenFromStorage } from "./shared/auth";
+
 const menu = reactive(menuobj);
 const appMenu = ref(null);
 
@@ -13,14 +16,34 @@ watch(route, (route) => {
     appMenu.value.routeChange(route);
   }
 });
+const authUserStore = useAuthUserStore();
+onMounted(async () => {
+  await authUserStore.fetchConfig();
+  appMenu.value.routeChange(route);
+});
+const logoutAction = () => {
+  authUserStore.$state.isUserLoggedin = false;
+  removeTokenFromStorage();
+  window.location.replace("/");
+};
 </script>
 
 <template>
-  <div v-if="menu">
-    <O4SAppMenu v-model="menu" ref="appMenu">
-      <template #app-page>
-        <RouterView />
-      </template>
-    </O4SAppMenu>
+  <div>
+    <div v-if="authUserStore.$state.isUserLoggedin">
+      <O4SAppMenu
+        v-model="menu"
+        ref="appMenu"
+        :profile="{
+          user: authUserStore.$state.user,
+          company: authUserStore.$state.company,
+        }"
+        @logout="logoutAction"
+      >
+        <template #app-page>
+          <RouterView />
+        </template>
+      </O4SAppMenu>
+    </div>
   </div>
 </template>
