7f8ec53f89b286bc2ffda3e6aac102be7c700b2d
shailendraO4S <102019685+shailendraO4S@users.noreply.github.com>
2022-12-27 14:48:33 +0530

    ++timeUtilsRefactor

--- dashboard-v2/src/utils/dateTime/index.js
+++ dashboard-v2/src/utils/dateTime/index.js
@@ -3,3 +3,12 @@ import { DateTime } from "luxon";
 export const getUnixTimeStamp = (date) => {
     return DateTime.fromJSDate(date).toMillis()
 }
+
+export const getFormatedDate = (date, type = null) => {
+    switch(type){
+        case "amPm":
+            return DateTime.fromMillis(date).toLocaleString(DateTime.DATETIME_MED)
+        default:
+            return DateTime.fromMillis(date).toFormat("LLL dd, yyyy")
+    }
+}
